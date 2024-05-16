864b36c253208a73f9203097e1fed26518967eeb
Justin Fant <justin.fant@bepcinc.com>
2024-05-16 08:43:46 -0500

    Updated gpt model to 4o

--- functions/gpt_call.py
+++ functions/gpt_call.py
@@ -8,7 +8,7 @@ client = OpenAI()
 
 def gpt_call(resume, schema):
   response = client.chat.completions.create(
-  model="gpt-3.5-turbo-0125",
+  model="gpt-4o",
   temperature=0,  
   seed= 123455555,
   response_format={"type": "json_object"},
